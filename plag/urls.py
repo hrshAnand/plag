53539781d026e576726c158b0050d21bdeb9fafc
harsh-not-haarsh <anand.2@iitj.ac.in>
2019-10-27 00:28:20 +0530

    added login/register

--- plag/urls.py
+++ plag/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from plagapp import views
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	url(r'^$',views.index,name='index'),
+	url(r'^special/',views.special,name='special'),
+	url(r'^plagapp/',include('plagapp.urls')),
+	url(r'^logout/$',views.user_logout,name='logout'),
 ]
--- plagapp/views.py
+++ plagapp/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from plagapp.forms import UserForm,UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+	return render(request,'plagapp/index.html')
+
+@login_required
+def special(request):
+	return HttpResponse("Logged in!")
+@login_required
+def user_logout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+	register=False
+	if request.method=='POST':
+		username=request.POST.get('username')
+		password=request.POST.get('password')
+		user=authenticate(username=username,password=password)
+		if user:
+			if user.is_active:
+				login(request,user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("account inactive")
+		else:
+			print("Someone tried to login and failed.")
+			print("username and password used: {} {}".format(username,password))
+			return HttpResponse("Invalid Details")
+	else:
+		return render(request,'plagapp/login.html',{})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, 'plagapp/login.html', {})
