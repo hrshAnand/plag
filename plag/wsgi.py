bfa179eab81bec552a47dbd85e44978048bcc6a1
harsh-not-haarsh <anand.2@iitj.ac.in>
2019-10-28 03:55:32 +0530

    changed settings

--- plag/wsgi.py
+++ plag/wsgi.py
@@ -15,3 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plag.settings')
 
 application = get_wsgi_application()
+application=DjangoWhiteNoise(application)
