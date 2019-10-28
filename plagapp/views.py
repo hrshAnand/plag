835c8d0300cce6217bb053c8b15855b825d75543
harsh-not-haarsh <anand.2@iitj.ac.in>
2019-10-28 05:50:20 +0530

    changes in registration form

--- plagapp/views.py
+++ plagapp/views.py
@@ -43,24 +43,20 @@ def register(request):
     registered = False
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
-        #profile_form = UserProfileInfoForm(data=request.POST)
+        user_form.username = request.POST.get('username')
+        user_form.password = request.POST.get('password')
         if user_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            #profile = profile_form.save(commit=False)
-            #profile.user = user
-            # if 'profile_pic' in request.FILES:
-            #     print('found it')
-            #     profile.profile_pic = request.FILES['profile_pic']
-            #profile.save()
             registered = True
+            return render(request,'plagapp/index.html')
         else:
-            print(user_form.errors,profile_form.errors)
+            print(user_form.errors)
     else:
         user_form = UserForm()
         #profile_form = UserProfileInfoForm()
-    return render(request,'plagapp/index.html',
+    return render(request,'plagapp/register.html',
                           {'user_form':user_form,
                            'registered':registered})
 
