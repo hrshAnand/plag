4a958dfca4cd21a0d8d6ca910b7308ac52f938f5
harsh-not-haarsh <anand.2@iitj.ac.in>
2021-01-05 01:54:34 +0530

    checking languages

--- plagapp/views.py
+++ plagapp/views.py
@@ -9,10 +9,9 @@ from django.contrib import messages
 import os
 
 
-
 def IndexView(request):
-    form = FileForm(request.POST, request.FILES)    
-    
+    form = FileForm(request.POST, request.FILES)
+
     if request.method == "POST":
         if True:
             print("we r in")
@@ -23,15 +22,30 @@ def IndexView(request):
             fs = FileSystemStorage()
             f1n = fs.save(f1.name, f1)
             f2n = fs.save(f2.name, f2)
-            
 
-            ls_fd = os.popen("cd media;chmod 0755 mossnet.pl;./mossnet.pl -l {} {} {} ".format(f1n.split('.')[-1],f1n, f2n))
+            lans = {
+                'c': 'c',
+                'cpp': 'cc',
+                'py': 'python',
+                'cs': 'csharp',
+                'js': 'javascript',
+            }
+
+            if f1n.split('.')[-1] != f2n.split('.')[-1]:
+                return HttpResponseRedirect("Files of different languages!")
+
+            ext = f2n.split('.')[-1]
+            if ext not in lans:
+                return HttpResponseRedirect(".{} files are not yet supported!\n You can only check plagiarism for the following files:{}".format(ext, lans.items()))
+
+            ls_fd = os.popen(
+                "cd media;chmod 0755 mossnet.pl;./mossnet.pl -l {} {} {} ".format(lans[ext], f1n, f2n))
             output = ls_fd.read()
             print(output)
             ls_fd.close()
-            ls_fd = os.popen("rm {};rm {};cd ..".format(f1n,f2n))
+            ls_fd = os.popen("cd media;rm {};rm {}".format(f1n, f2n))
             ls_fd.close()
             output = str(output).split()
             return HttpResponseRedirect(output[0])
 
-    return render(request, 'plagapp/index.html', {"form" : FileForm()})
+    return render(request, 'plagapp/index.html', {"form": FileForm()})
